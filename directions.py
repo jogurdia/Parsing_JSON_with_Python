3e208d8bb331b98c5331417dce290ed609411a5e
Johan Gurdian <jogurdia@cisco.com>
2020-02-07 23:39:03 -0600

    removed fixed location and add while loop to ask for directions

--- directions.py
+++ directions.py
@@ -5,34 +5,28 @@ import urllib.parse
 import requests
 
 # Create RESTful API call to MapQuest...
-
 # Create variable for base URL 
-
 base_api_url = "http://www.mapquestapi.com/directions/v2/route?"
-start_location = "Baltimore"
-dst_location = "Miami"
+#start_location = "Baltimore"
+#dst_location = "Miami"
 api_key = "wUVBAWuwYk7FTz8JYOexkXcO3QDobmOB"
-
+while True:
+    start_location = input("Please enter location: ")
+    dst_location = input("Please enter destination: ")
 # Create the final URL with urlencode form urllib
-
-url = base_api_url + urllib.parse.urlencode({"key":api_key, "from":start_location, "to":dst_location})
-
+    url = base_api_url + urllib.parse.urlencode({"key":api_key, "from":start_location, "to":dst_location})
 # Sending a get request from variable url and requesting JSON data format
-
-json_response = requests.get(url).json()
-
-print(json_response)
-print(url)
-
+    json_response = requests.get(url).json()
+    print(json_response)
+    print(url)
 # Check status code of call to RESTful API...
-
-returned_status = json_response["info"]["statuscode"]
-mapquest_copyright = json_response["info"]["copyright"]["text"]
-print()
-if returned_status == 0:
-    print("The status of your API call was: " + str(returned_status))
-    print(mapquest_copyright)
-print()
+    returned_status = json_response["info"]["statuscode"]
+    mapquest_copyright = json_response["info"]["copyright"]["text"]
+    print()
+    if returned_status == 0:
+        print("The status of your API call was: " + str(returned_status))
+        print(mapquest_copyright)
+    print()
 
 
 
