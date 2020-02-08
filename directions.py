3d08a32a5f9d4b6d7101c531da97043c07b67a4d
Johan Gurdian <jogurdia@cisco.com>
2020-02-08 00:27:42 -0600

    Added distance and fuel

--- directions.py
+++ directions.py
@@ -9,16 +9,27 @@ import requests
 base_api_url = "http://www.mapquestapi.com/directions/v2/route?"
 #start_location = "Baltimore"
 #dst_location = "Miami"
+
 api_key = "wUVBAWuwYk7FTz8JYOexkXcO3QDobmOB"
+
 while True:
-    start_location = input("Please enter location: ")
-    dst_location = input("Please enter destination: ")
+    start_location = input("Please enter location or 'q' to quit: ")
+    if start_location == 'q':
+        break
+
+    dst_location = input("Please enter destination or 'q' to quit: ")
+    if dst_location == 'q':
+        break
+
 # Create the final URL with urlencode form urllib
     url = base_api_url + urllib.parse.urlencode({"key":api_key, "from":start_location, "to":dst_location})
+
 # Sending a get request from variable url and requesting JSON data format
     json_response = requests.get(url).json()
-    print(json_response)
+ #   print(json_response)
+    print()
     print(url)
+
 # Check status code of call to RESTful API...
     returned_status = json_response["info"]["statuscode"]
     mapquest_copyright = json_response["info"]["copyright"]["text"]
@@ -26,6 +37,11 @@ while True:
     if returned_status == 0:
         print("The status of your API call was: " + str(returned_status))
         print(mapquest_copyright)
+        print("##############################################################")
+        print("For your directions the following information was found: ")
+        print("The Total Distance is: " + str(json_response["route"]["distance"]))
+        print("The total amount of fuel used would be: " +str(json_response["route"]["fuelUsed"]))
+        print("##############################################################")    
     print()
 
 
